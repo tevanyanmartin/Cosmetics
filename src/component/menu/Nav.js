533b23956f1aac4d86db4021e5eb1127e05314c2
tevanyanmartin <tevanyanmartin@outlook.com>
2021-03-27 23:18:02 +0400

    4

--- src/component/menu/Nav.js
+++ src/component/menu/Nav.js
@@ -1,12 +1,11 @@
 // import "../../styles/styles.css";
 import '../../styles/nav.css'
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import wLogo from "../../Pics/white-logo.png";
 import Navmodules from './Navmodules'
-import { useHistory } from "react-router-dom";
 import { auth } from '../../index'
-function Nav(props) {
+function Nav() {
   const [displayNone, setDisplay] = useState('none')
   const handleToggle = (e) => {
     if (displayNone === 'block') {
@@ -14,85 +13,43 @@ function Nav(props) {
     } else {
       setDisplay('block')
     }
-
   }
-
-  var user = auth.currentUser;
-  var name, email, photoUrl, uid, emailVerified;
+  let user = auth.currentUser;
+  let email;
   if (user != null) {
-    name = user.displayName;
     email = user.email;
-    photoUrl = user.photoURL;
-    emailVerified = user.emailVerified;
-    uid = user.uid;
   }
- 
- console.log(name, email, photoUrl, uid, emailVerified)
- 
-
- 
-
   let manuUserSeting;
-
   if (email) {
-
     manuUserSeting = <span onClick={handleToggle} id='profile-items' className='profile-items-span'>{email} </span>
   } else {
     manuUserSeting = <span onClick={handleToggle} id='profile-items' className='profile-items-span' >My account</span>
   }
-  const history = useHistory()
-  const kkk = () => {
 
-    history.push('/eyes')
-    return (
-      <h1> heeelloooo neeeww</h1>
-    )
-
-  }
   return (
-
-      <div className='page-navigation'>
-        <div className='page-container'>
-          <div className='navbar-mobile'></div>
-          <div className='navbar-desktop'>
-            <a className='navbar-desktop-a' href="/"><img alt='img' src={wLogo} /></a>
-            <div className='navbar-menu'>
-
-              <a onClick={kkk}>asdasd</a>
-              <Link className='navbar-menu-a' to="/new">
-                New
-                </Link>
-              <Link className='navbar-menu-a' to="/lips">
-                Lips
-                </Link>
-              <Link className='navbar-menu-a' to="/face">
-                Face
-                </Link>
-              <Link className='navbar-menu-a' to="/eyes">
-                Eyes
-                </Link>
-              <Link className='navbar-menu-a' to="/brushes">
-                Brushes
-                </Link>
-              <Link className='navbar-menu-a' to="/skin">
-                Skin
-                </Link>
-              <Link className='navbar-menu-a' to="/bestSellers">
-                BEST-SELLERS
-                </Link>
-              <Link className='navbar-menu-a' to="/brands">
-                Brands
-                </Link>
-
-            </div>
-            <div className='profile-items'>
-              {manuUserSeting}
-            </div>
+    <div className='page-navigation'>
+      <div className='page-container'>
+        <div className='navbar-mobile'></div>
+        <div className='navbar-desktop'>
+          <a className='navbar-desktop-a' href="/"><img alt='img' src={wLogo} /></a>
+          <div className='navbar-menu'>
+            <Link className='navbar-menu-a' to="/new">New</Link>
+            <Link className='navbar-menu-a' to="/lips">Lips</Link>
+            <Link className='navbar-menu-a' to="/face">Face</Link>
+            <Link className='navbar-menu-a' to="/eyes">Eyes</Link>
+            <Link className='navbar-menu-a' to="/brushes">Brushes</Link>
+            <Link className='navbar-menu-a' to="/skin">Skin</Link>
+            <Link className='navbar-menu-a' to="/bestSellers">BEST-SELLERS</Link>
+            <Link className='navbar-menu-a' to="/brands">Brands</Link>
           </div>
-          <Navmodules changeProfileDisplay={displayNone} handleToggle={handleToggle} />
+          <div className='profile-items'>
+            {manuUserSeting}
+          </div>
         </div>
-
+        <Navmodules changeProfileDisplay={displayNone} handleToggle={handleToggle} />
       </div>
+
+    </div>
 
   )
 }
--- src/component/menu/Navmodules.js
+++ src/component/menu/Navmodules.js
@@ -1,157 +1,61 @@
-
 import '../../styles/navModules.css'
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import addUserIcon from '../../Pics/icons/addUser.png'
-import { BrowserRouter as Router, Switch, Route, Link, useHistory } from "react-router-dom";
+import { BrowserRouter as  Switch, Route, Link } from "react-router-dom";
 import Login from './Login'
 import Signup from './Signup';
 import { auth, db } from '../../index'
 
 const Menumodules = (props) => {
-
-    const history = useHistory()
-    let user = auth.currentUser;
-    let name, email, photoUrl, uid, emailVerified;
-    let img = '';
-    if (user != null) {
-        name = user.displayName;
-        email = user.email;
-        photoUrl = user.photoURL;
-        emailVerified = user.emailVerified;
-        uid = user.uid;
-        db.collection("users").doc(user.uid).get().then((doc) => {
-            if (doc.exists) {
-                img = doc.data().email
-                console.log(img);
-            } else {
-                // doc.data() will be undefined in this case
-                console.log("No such document!");
-            }
-        }).catch((error) => {
-            console.log("Error getting document:", error);
-        });
-    }
-   
-
-
-    console.log(name, email, photoUrl, uid, emailVerified)
-
-    useEffect(() => {
-        console.log(img);
-    }, [user])
-    
-    const handleLogout = () => {
-        auth.signOut();
-    }
-
-    let manuUserSeting;
-
-    if (email) {
-        manuUserSeting = <div style={{ display: props.changeProfileDisplay }} className='profile-dropdown'>
-            <div className='profile-dropdown-contant'>
-                <div className='profile-dropdown-info profile-items'>
-                    <span className='profile-dropdown-info-img'><img alt='img' src={img} />{img}</span>
-                    <span className='profile-dropdown-info-name'>{email}</span>
-                </div>
-                <div className='profile-dropdown-logout'><span onClick={handleLogout} className='profile-dropdown-logout-button'>LogOut</span></div>
-            </div>
-
-import "../../styles/navModules.css";
-import React, { useState, useEffect } from "react";
-import addUserIcon from "../../Pics/icons/addUser.png";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Login from "./Login";
-import Signup from "./Signup";
-import { auth } from "../../index";
-const Menumodules = (props) => {
-  var user = auth.currentUser;
-  var name, email, photoUrl, uid, emailVerified;
+  let user = auth.currentUser;
+  let email, uid;
+  const [userImg, setImg] = useState('');
   if (user != null) {
-    name = user.displayName;
     email = user.email;
-    photoUrl = user.photoURL;
-    emailVerified = user.emailVerified;
     uid = user.uid;
+    db.collection("users").doc(uid).get().then((doc) => {
+    if (doc.exists) {
+      setImg(doc.data().image)
+    } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
+    }
+  }).catch((error) => {
+    console.log("Error getting document:", error);
+  });
   }
-
-  console.log(name, email, photoUrl, uid, emailVerified);
-
+  
   const handleLogout = () => {
     auth.signOut();
-  };
-
+    window.location.reload()
+  }
   let manuUserSeting;
   if (email) {
-    manuUserSeting = (
-      <div
-        style={{ display: props.changeProfileDisplay }}
-        className="profile-dropdown"
-      >
-        <div className="profile-dropdown-contant">
-          <div className="profile-dropdown-info profile-items">
-            <span className="profile-dropdown-info-img">
-              <img alt="img" src={""} />
-            </span>
-            <span className="profile-dropdown-info-name">{user.email}</span>
-          </div>
-          <div className="profile-dropdown-logout">
-            <span
-              onClick={handleLogout}
-              className="profile-dropdown-logout-button"
-            >
-              LogOut
-            </span>
-          </div>
-
+    manuUserSeting = <div style={{ display: props.changeProfileDisplay }} className='profile-dropdown'>
+      <div className='profile-dropdown-contant'>
+        <div className='profile-dropdown-info profile-items'>
+          <span className='profile-dropdown-info-img'><img alt='img' src={userImg} />{userImg}</span>
+          <span className='profile-dropdown-info-name'>{email}</span>
+        </div>
+        <div className='profile-dropdown-logout'><span onClick={handleLogout} className='profile-dropdown-logout-button'>LogOut</span></div>
+      </div>
+    </div>
+  } 
+  else {
+    manuUserSeting = <div style={{ display: props.changeProfileDisplay }} className="profile-dropdown">
+      <div className="profile-dropdown-contant">
+        <div className="navbar-loginButtonLink">
+          <Link id="loginButtonLink" to="/login">Login</Link>
+        </div>
+        <div className="navbar-createAccountButtonLink">
+          <span>
+            <img src={addUserIcon} alt="img" />
+          </span>
+          <Link id="createAccountButtonLink" to="signup">Create New Account</Link>
         </div>
       </div>
-    );
-  } else {
-    manuUserSeting = (
-      <div
-        style={{ display: props.changeProfileDisplay }}
-        className="profile-dropdown"
-      >
-        <div className="profile-dropdown-contant">
-          <div className="navbar-loginButtonLink">
-            <Link id="loginButtonLink" to="/login">
-              Login
-            </Link>
-          </div>
-          <div className="navbar-createAccountButtonLink">
-            <span>
-              <img src={addUserIcon} alt="img" />
-            </span>
-            <Link id="createAccountButtonLink" to="signup">
-              Create New Account
-            </Link>
-          </div>
-        </div>
-
-    }
-
-
-
-
-
-    return (
-        <>
-            <div onClick={props.handleToggle} style={{ display: props.changeProfileDisplay }} className='close-dropDown'></div>
-            <Switch>
-                <Route path="/login" component={Login} />
-                <Route path="/signup" component={Signup} />
-            </Switch>
-            {manuUserSeting}
-        </>
-    )
-}
-
-export default Menumodules
-
-      </div>
-    );
-  }
-  return (
+    </div>
+  } return (
     <>
       <div
         onClick={props.handleToggle}
