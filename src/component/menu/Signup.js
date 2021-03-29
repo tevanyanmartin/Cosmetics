9cddb7a20f278119c28269af4f43e0d69068feff
Siirusho <74008883+Siirusho@users.noreply.github.com>
2021-03-29 21:13:10 +0400

    nested router

--- src/component/menu/Signup.js
+++ src/component/menu/Signup.js
@@ -17,7 +17,7 @@ const Signup = () => {
     });
     const generatePreviewImgUrl = (file, callback) => {
         const reader = new FileReader()
-        reader.readAsDataURL(file)
+        const url = reader.readAsDataURL(file)
         reader.onloadend = e => callback(reader.result)
     }
     let inputChange = { ...values }
@@ -52,6 +52,7 @@ const Signup = () => {
         auth.createUserWithEmailAndPassword(values.email, values.password)
             .then((userCredential) => {
                 let user = userCredential.user;
+                console.log(user.uid)
                 db.collection("users").doc(user.uid).set({
                     // uid: user.uid,
                     username: inputChange.username,
